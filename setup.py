eb87ee3affc2e4747c8f22411d9239784eead6f0
Devon Meunier <devon.meunier@utoronto.ca>
2011-04-13 22:57:48 -0400

    Added distutils script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "python-ips",
+      version = '1.0',
+      description = "Python IPS patcher.",
+      author = "Devon Meunier",
+      license = "MIT License",
+      author_email = "devon.meunier@utoronto.ca",
+      py_modules = ['ips'])
