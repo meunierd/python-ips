913eb74ad7521f22f2396fc470ad7efd68bccc57
Devon Meunier <devon.meunier@utoronto.ca>
2012-01-07 19:09:41 -0500

    Implements DvD's ROM Expander Pro format, less the replace feature. Just place the ROM Expander Pro.txt file in the same directory as expansion.py with the ROM renamed to whatever it says in the script, and run expansion.py

---
+++ expansion.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+from binascii import unhexlify
+from struct import unpack_from
+import md5
+
+__author__ = "Devon Meunier"
+__email__ = "devon.meunier@utoronto.ca"
+__license__ = "MIT"
+__version__ = "0.1"
+
+def get_md5(source):
+    """Return the MD5 hash of the file `source`."""
+    m = md5.new()
+    while True:
+        d = source.read(8096)
+        if not d: break
+        m.update(d)
+    return m.hexdigest()
+
+def hex_to_bstr(d):
+    """Return the bytestring equivalent of a plain-text hex value."""
+    if len(d) % 2: d = "0" + d
+    return unhexlify(d)
+
+def bstr_to_long(b):
+    """Return a bytestring as a number."""
+    b = "\x00" * (4 - len(b)) + b
+    return unpack_from(">l", b)[0]
+
+def load_line(s):
+    """Tokenize a tab-delineated string and return as a list."""
+    return s.strip().split('\t')
+
+def load_script():
+    script = {}
+    script["file"] = "ROM Expander Pro.txt"
+    with open(script["file"]) as script_file:
+        script_lines = script_file.readlines()
+
+    # Load the `NAME` line from script.
+    l = load_line(script_lines.pop(0))
+    assert 'NAME' == l.pop(0)
+    script["source"], script["target"] = l
+    assert script["target"] != script["source"]
+
+    # Load the `SIZE` and optional `MD5`
+    # In the original ROM Expander Pro, an MD5 is checked if present, but
+    # will attempt to expand anyways. This implementation forces MD5s to
+    # match if present.
+    l = load_line(script_lines.pop(0))
+    script["old_size"] = bstr_to_long(hex_to_bstr(l[1]))
+    script["new_size"] = bstr_to_long(hex_to_bstr(l[2]))
+    if l.index(l[-1]) > 2: script["MD5"] = l[3].lower()
+
+    # Load the replacement `HEADER`.
+    l = load_line(script_lines.pop(0))
+    assert 'HEADER' == l.pop(0) 
+    script["header_size"] = bstr_to_long(hex_to_bstr(l.pop(0)))
+    assert script["header_size"] > len(l)
+    # Sanitize and concatenate the header data.
+    new_header = "".join(["0" * (2 - len(x)) + x for x in l])
+    # Cast to character data and pad with 0x00 to header_size
+    new_header = hex_to_bstr(new_header)
+    script["header"] = new_header + "\x00" * (script["header_size"] - len(l))
+
+    # Check the source file MD5.
+    if "MD5" in script:
+        with open(script["source"], "rb") as s_file:
+            # Don't digest the header.
+            s_file.read(script["header_size"])
+            assert script["MD5"] == get_md5(s_file)
+
+    script["ops"] = []
+    while script_lines:
+        script["ops"].append(load_line(script_lines.pop(0)))
+
+    return script
+
+def expand_rom(script):
+    with open(script["source"], "rb") as s, open(script["target"], "wb") as t:
+
+        def copy(a, b):
+            source_ptr = script["header_size"] + a
+            write_ptr = script["header_size"] + b
+            s.seek(source_ptr)
+            t.seek(write_ptr)
+            t.write(s.read(end_ptr - write_ptr))
+
+        def replace(a, b):
+            pass # maybe do this in a second pass
+
+        def fill(destination, value):
+            write_ptr = script["header_size"] + destination
+            t.seek(write_ptr)
+            t.write(value * (end_ptr - write_ptr))
+            
+        # Write Header
+        t.write(script["header"])
+
+        while script["ops"]:
+            op = script["ops"].pop(0)
+            cmd = op.pop(0)
+
+            if not script["ops"]:
+                end_ptr = script["header_size"] + script["new_size"]
+            else:
+                end_ptr = bstr_to_long(hex_to_bstr(script["ops"][0][1])) + \
+                          script["header_size"]
+
+            if cmd == "COPY":
+                copy(bstr_to_long(hex_to_bstr(op[1])), 
+                     bstr_to_long(hex_to_bstr(op[0])))
+           
+            elif cmd == "FILL":
+                fill(bstr_to_long(hex_to_bstr(op[0])),
+                     hex_to_bstr(op[1]))
+
+            elif cmd == "REPLACE": pass
+
+script = load_script()
+expand_rom(script)
